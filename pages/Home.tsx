7bc8218ddb15d30b7c102596facfe05b6e90e3a5
sakshi <sakshisawant413@gmail.com>
2025-12-16 18:14:16 +0530

    home_imgs

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -184,7 +184,7 @@ export const Home: React.FC<HomeProps> = ({ onNavigate }) => {
                <div className="rounded-2xl overflow-hidden mb-6 relative">
                  <div className="absolute inset-0 bg-neutral/0 group-hover:bg-neutral/10 transition-colors z-10"></div>
                  <img 
-                    src="/Images/pexels-iamluisao-12917374.jpg" 
+                    src="/Images/pediatric.jpg" 
                     alt="Pediatric" 
                     className="w-full aspect-[4/3] object-cover group-hover:scale-105 transition-transform duration-700" 
                  />
